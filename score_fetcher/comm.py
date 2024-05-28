8a6875bc54da59cefaa2f6f3037689065610cf1a
Loic Brasey <loibrasey@heig-vd.ch>
2024-05-28 12:02:28 +0200

    post

--- score_fetcher/comm.py
+++ score_fetcher/comm.py
@@ -12,11 +12,22 @@ scoreboard = {
 
 class HTTPRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-        if re.search('/api/post/score', self.path):
+        if re.search('/api/post/reset', self.path):
             length = int(self.headers.get('content-length'))
-            data = self.rfile.read(length).decode('utf8')
+            #data = self.rfile.read(length).decode('utf8')
 
+            # reset the scoreboard
+            for key in scoreboard.keys() :
+                scoreboard[key] = 0
+
+            # re
             self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+
+            # Return json, even though it came in as POST URL params
+            data = json.dumps(scoreboard).encode('utf-8')
+            self.wfile.write(data)
         else:
             self.send_response(403)
         self.end_headers()
@@ -25,7 +36,6 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         if re.search('/api/get/score', self.path):
                 self.send_response(200)
                 self.send_header('Content-Type', 'application/json')
-                self.send_header('Access-Control-Allow-Origin', '*')
                 self.end_headers()
 
                 # Return json, even though it came in as POST URL params
